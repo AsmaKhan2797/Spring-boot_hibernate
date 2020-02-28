9acf846a0a8e1101f4e3db8a49329e55fa3d55bb
AsmaKhan2797 <asmakhan1427@gmail.com>
2020-02-28 19:11:25 +0530

    added phone-number formatter code

--- src/main/java/basicWebAppInMaven/maven/App.java
+++ src/main/java/basicWebAppInMaven/maven/App.java
@@ -1,47 +1,56 @@
 package basicWebAppInMaven.maven;
 
+public class App {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 5, 4, 7, 8, 9, 0, 6 };
+		String res = formatPhoneNumber(arr);
+		System.out.println("res  :" + res);
+	}
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	String res=test();
-    	System.out.println("================"+res);
-       
-    }
-    @SuppressWarnings("finally")
-	public static String test() {
-    	String ress="N";
-    	
-    	//int i=0;
-    	try {
-   		 System.out.println( "Hello World!" );
-   		ress= "Y";
-   		String a ="abc";
-   		String ab ="ab"+"c";
-   		System.out.println(a!=ab);
-   	/*	if(i) {System.out.println("true");}
-   		else {}*/
-   		 return ress;
-   	}
-   	
-   	catch(Exception e) {
-   		System.out.println("e" +e.getMessage());
-   		return ress;
-   	}
-   	
-   	finally {
-   		System.out.println("finally ........");
-   		try {
-      		 System.out.println( "Hello World!" );
-      		return ress;
-      	}
-      	
-      	catch(Exception e) {
-      		System.out.println("e" +e.getMessage());
-      		return ress;
-      	}
-   		
-   	}
-    }
+	public static String formatPhoneNumber(int[] nums) {
+		int[] arr = nums;
+		if (arr.length != 10) {
+			return "false";
+		} else {
+			String final_result = null;
+			String result = "(";
+			for (int i = 0; i < arr.length; i++) {
+
+				if (i < 3) {
+					result = result + arr[i];
+					// System.out.println("result :"+result);
+				} else if (i == 3) {
+					result = result + ") ";
+					final_result = result + arr[i];
+					// System.out.println("final_result 1 :"+final_result);
+				}
+
+				else if (i < 6) {
+
+					final_result = final_result + arr[i];
+
+					System.out.println("final_result  :" + final_result);
+
+				} else if (i == 6) {
+					final_result = final_result + "-";
+					final_result = final_result + arr[i];
+					// System.out.println("final_result 1 :"+final_result);
+				} else {
+					final_result = final_result + arr[i];
+					// System.out.println("final_result :"+final_result);
+					
+				}
+				
+			}
+			String s = "abc";
+			String sb = new String("abc");
+
+			System.out.println(" check  :" + (s == sb));
+			System.out.println("equals :"+s.equals(sb));
+//return final_result;
+			return String.format("(%d%d%d) %d%d%d-%d%d%d%d", nums[0], nums[1], nums[2], nums[3], nums[4], nums[5],
+					nums[6], nums[7], nums[8], nums[9]);
+		}
+	}
+
 }
