d4a55be8f3037b6b834180a7dbbd88a980b37bf0
asma3608@ISGMGORD120 <asmakhan1427@gmail.com>
2020-05-28 22:50:43 +0530

    logs changes

--- src/main/java/basic/com/test/entry_point.java
+++ src/main/java/basic/com/test/entry_point.java
@@ -3,14 +3,21 @@ package basic.com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class entry_point {
-
+public class entry_point{ //extends SpringBootServletInitializer {
+	/*@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+	    return builder.sources(entry_point.class);
+	  }
+*/
 	public static void main(String[] args) {
 		SpringApplication.run(entry_point.class, args);
-		/*System.out.println("running........");*/
+		System.out.println("running........");
+		
 	}
 
 	
